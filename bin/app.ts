942c2af7622ab2e78881394a0086cc9f243afbe0
kashio peiya <multistriata@gmail.com>
2024-03-24 18:53:04 +0900

    feat: 不要リソース削除

--- bin/app.ts
+++ bin/app.ts
@@ -7,22 +7,15 @@ import { DevStage } from '../lib/stage/devStage'
 import { ProdStage } from '../lib/stage/prodStage'
 import { devConfig, prodConfig } from '../config'
 import { nagSuppressions } from './nagSuppressions'
-import { CdkPipelineStack } from '../lib/stack/cdkPipelineStack'
 
 const app = new cdk.App()
 
-const devStage_ = new DevStage(app, 'dev', {
+new DevStage(app, 'dev', {
   env: devConfig.env
 })
 
-const prodStage_ = new ProdStage(app, 'prod', {
+new ProdStage(app, 'prod', {
   env: prodConfig.env
-})
-
-new CdkPipelineStack(app, 'kasio-dev-pipeline-stack', {
-  env: devConfig.env,
-  devStage: devStage_,
-  prodStage: prodStage_
 })
 
 Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }))

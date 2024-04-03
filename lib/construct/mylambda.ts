3454f679ed9f34a1a2217cf2d14029e194c000eb
kashio peiya <multistriata@gmail.com>
2024-04-04 08:10:16 +0900

    fix: Lambda Layerのimportエラー修正

--- lib/construct/mylambda.ts
+++ lib/construct/mylambda.ts
@@ -20,12 +20,21 @@ export class MyNodejsFunction extends Construct {
   constructor(scope: Construct, id: string, props: MyNodejsFunctionProps) {
     super(scope, id)
 
+    // Lambda Layer
+    const layer = new lambda_.LayerVersion(this, 'LambdaLayer', {
+      removalPolicy: RemovalPolicy.DESTROY,
+      code: lambda_.Code.fromAsset('resources/layer/logging'),
+      compatibleRuntimes: [lambda_.Runtime.NODEJS_20_X],
+      compatibleArchitectures: [lambda_.Architecture.ARM_64]
+    })
+
     // Lambda Function
     this.func = new NodejsFunction(this, 'LambdaFunc', {
       runtime: lambda_.Runtime.NODEJS_20_X,
       architecture: lambda_.Architecture.ARM_64,
       ...props.lambda
     })
+    this.func.addLayers(layer)
 
     // CloudWatch Logs: LogGroup
     this.logGroup = new LogGroup(this, 'LogGroup', {
